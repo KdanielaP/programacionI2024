817872f4a0f8501b70e6dec2038fa222cac17ddf
KdanielaP <135910567+KdanielaP@users.noreply.github.com>
2024-03-19 21:56:32 -0600

    nueva actualizacion

---
+++ arregloclase/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace sesion7
+{
+
+
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            int[] arreglo = { 1, 2, 3 };
+            int[] arreglo2 = new int[10];
+            int[] arreglo3 = new int[4] { 1, 2, 4, 3 };
+            // gyardar valores en arreglo 
+            arreglo2[0] = 1;
+            Console.WriteLine(arreglo2[0]);
+           
+            for (int i = 0; i < arreglo2.Length; i++)
+            {
+                arreglo2[i] = i + 1;
+
+                Console.WriteLine(arreglo2[i]);
+
+                int contador = 0;
+                while (contador < arreglo2.Length)
+                {
+                 
+                    Console.WriteLine(arreglo2[contador]);
+                    contador++;
+                }
+            }
+
+        }
+    }
+
+}
---
+++ totito/Totito.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Totito
+{
+    class Program
+    {
+        static char[,] board = new char[3, 3] {
+            { ' ', ' ', ' ' },
+            { ' ', ' ', ' ' },
+            { ' ', ' ', ' ' }
+        };
+
+        static char currentPlayer = 'X';
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                DrawBoard();
+
+                Console.WriteLine($"Turno del jugador {currentPlayer}:");
+
+                int row, col;
+                do
+                {
+                    Console.WriteLine("Ingresa la fila y columna (ejemplo: 1 2):");
+                    string[] input = Console.ReadLine().Split();
+                    row = int.Parse(input[0]) - 1;
+                    col = int.Parse(input[1]) - 1;
+                } while (row < 0 || row > 2 || col < 0 || col > 2 || board[row, col] != ' ');
+
+                board[row, col] = currentPlayer;
+
+                if (CheckWinner())
+                {
+                    Console.Clear();
+                    DrawBoard();
+                    Console.WriteLine($"El jugador {currentPlayer} ha ganado!");
+                    break;
+                }
+
+                if (IsBoardFull())
+                {
+                    Console.Clear();
+                    DrawBoard();
+                    Console.WriteLine("¡Empate!");
+                    break;
+                }
+
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        }
+
+        static void DrawBoard()
+        {
+            Console.WriteLine("  1 2 3");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write((i + 1) + " ");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool CheckWinner()
+        {
+            // Verificar filas, columnas y diagonales
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == currentPlayer && board[i, 1] == currentPlayer && board[i, 2] == currentPlayer)
+                    return true;
+                if (board[0, i] == currentPlayer && board[1, i] == currentPlayer && board[2, i] == currentPlayer)
+                    return true;
+            }
+
+            if ((board[0, 0] == currentPlayer && board[1, 1] == currentPlayer && board[2, 2] == currentPlayer) ||
+                (board[0, 2] == currentPlayer && board[1, 1] == currentPlayer && board[2, 0] == currentPlayer))
+                return true;
+
+            return false;
+        }
+
+        static bool IsBoardFull()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == ' ')
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
